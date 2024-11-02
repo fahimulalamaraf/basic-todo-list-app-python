d13bd499677187755ee034e5a94720b5dbf87981
Fahimul Alam <fahimul18alam@gmail.com>
2024-11-02 16:58:26 +0600

    Updated with Function for code redundancy

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 
+def get_todos():
+    with open('todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -9,8 +15,7 @@ while True:
 
         todo = user_action[4:]
 
-        with open('todos.txt','r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
@@ -20,9 +25,7 @@ while True:
 
     elif user_action.startswith("show"):
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.title()
@@ -43,8 +46,7 @@ while True:
             todos[number] = edited_user_action
 
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            todos = get_todos()
 
             print("Updated Successfully!")
 
